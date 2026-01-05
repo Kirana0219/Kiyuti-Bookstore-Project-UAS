60e93585e23accd6ee1db8fd304b87b581d49b98
Putu Kartika <kartikaputu410@gmail.com>
2026-01-05 16:30:51 +0800

    memperbaiki bagian kode agar jumlah buku bisa ditambahkan

--- JS/apps.js
+++ JS/apps.js
@@ -247,16 +247,17 @@ document.querySelectorAll(".book-card").forEach(card => {
     modalDesc.textContent = card.querySelector(".book-desc").textContent;
     modalPrice.textContent = card.querySelector(".book-price").textContent;
 
-    modalImg.src = card.dataset.img;
-
-    currentStock = parseInt(card.dataset.stock); modalStock.textContent = isNaN(currentStock) ? "10" : currentStock;
-
+    // Ambil gambar langsung dari <img>
     modalImg.src = card.querySelector("img").src;
 
+    // Ambil stok, default 10 kalau kosong
+    currentStock = parseInt(card.dataset.stock) || 10;
+    modalStock.textContent = currentStock;
 
     modal.style.display = "flex";
   });
 });
+
 
 // Kontrol kuantitas
 plusBtn.addEventListener("click", () => {
